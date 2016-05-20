1385dab99e1d90563757b26fa771cd3988b06135
moller-g-peter <moller.g.peter@gmail.com>
2016-05-20 20:10:07 +0200

    start working on the JSON file for all zodiacs

--- js/moon_dial.js
+++ js/moon_dial.js
@@ -3,118 +3,111 @@ $('.sunY_high').hide();
 
 
 
-function star1_1(){
-  $('.star1_1').css('z-index','2');
-  $('.star1_2').css('z-index','1').fadeIn(1000);
-  $('.star1_1').fadeOut(1000, function(){
-    star1_2();
-  });
+setInterval( function() {
+  var seconds = new Date().getSeconds();
+  var sdegree = seconds * 6;
+  var srotate = "rotate(" + sdegree + "deg)";
+  // console.log("srotate :", srotate);
+  $("#sec").css({ "transform": srotate });
+
+}, 1000 );
+// -----------------------------------------------
+
+
+
+setInterval( function() {
+  var hours = new Date().getHours();
+  var mins = new Date().getMinutes();
+  var hdegree = hours * 30 + (mins / 2);
+  var hrotate = "rotate(" + hdegree + "deg)";
+
+
+// equation for the disc (halfspeed)
+  var halfDegree = hours * 30 + (mins / 2) / 2;
+  var halfRotate = "rotate(" + halfDegree + "deg)";
+  $("#gradientDisc").css({ "transform": halfRotate });
+  // console.log("halfDegree :", halfDegree);
+  // console.log("halfRotate :", halfRotate);
+  // console.log("hrotate :", hrotate);
+
+
+  $("#hour").css({ "transform": hrotate});
+    
+}, 1000 );
+// -----------------------------------------------
+
+
+
+setInterval( function() {
+  var mins = new Date().getMinutes();
+  var mdegree = mins * 6;
+  var mrotate = "rotate(" + mdegree + "deg)";
+
+// equation for the disc (halfspeed)
+  // var halfDegree = mins * 12 / 2;
+  // var halfRotate = "rotate(" + halfDegree + "deg)";
+
+  $("#min").css({ "transform" : mrotate });
+  // $("#gradientDisc").css({ "transform": halfRotate });
+  // console.log('number :', number);
+
+}, 1000 );
+// -----------------------------------------------
+
+
+
+setInterval( function() {
+  var mins = new Date().getMinutes();
+  var mdegree = mins * 6;
+  var mrotate = "rotate(" + mdegree + "deg)";
+
+   // remove string from 'mrotate' and keep the integer
+  var number = mrotate.replace(/[^0-9]/g, '');
+
+  if (number >= 120 && number <= 270){
+    $(".shootingStar").animate({left: '-100', top: '140'}, 700, 'linear', function(){
+      $('.shootingStar').css({left:467, top:437});
+    });
+  }
+}, 20000 );
+
+
+
+
+
+function setStarTwinkleInterval (starClass, fadeTime) {
+  setInterval( function () {
+    // console.log('it works');
+    twinkleStar(starClass, fadeTime);
+  }, fadeTime);
 }
-function star1_2(){
-  $('.star1_2').css('z-index','2');
-  $('.star1_1').css('z-index','1').fadeIn(1000);
-  $('.star1_2').fadeOut(1000, function(){
-    star1_1();
-  });
+
+function twinkleStar(starClass, fadeTime) {
+  // console.log(starClass, fadeTime);
+  // console.log('it works');
+  var starPart1 = $('.' + starClass + '_1');
+  var starPart2 = $('.' + starClass + '_2');
+
+  // console.log(starPart1.css('z-index'));
+  // console.log(starPart2.css('z-index'));
+
+  if (starPart1.css('z-index') == 1) {
+    starPart1.css('z-index', '2').fadeOut(fadeTime);
+    starPart2.css('z-index', '1').fadeIn(fadeTime);
+  }
+  else if (starPart1.css('z-index') == 2) {
+    starPart1.css('z-index', '1').fadeIn(fadeTime);
+    starPart2.css('z-index', '2').fadeOut(fadeTime);
+  }
+  else {
+    starPart1.css('z-index', '2').fadeOut(fadeTime);
+    starPart2.css('z-index', '1').fadeIn(fadeTime);
+  }
+  // console.log('fart');
 }
-function star2_1(){
-  $('.star2_1').css('z-index','2');
-  $('.star2_2').css('z-index','1').fadeIn(1300);
-  $('.star2_1').fadeOut(1300, function(){
-    star2_2();
-  });
-}
-function star2_2(){
-  $('.star2_2').css('z-index','2');
-  $('.star2_1').css('z-index','1').fadeIn(1300);
-  $('.star2_2').fadeOut(1300, function(){
-    star2_1();
-  });
-}
-function star3_1(){
-  $('.star3_1').css('z-index','2');
-  $('.star3_2').css('z-index','1').fadeIn(700);
-  $('.star3_1').fadeOut(700, function(){
-    star3_2();
-  });
-}
-function star3_2(){
-  $('.star3_2').css('z-index','2');
-  $('.star3_1').css('z-index','1').fadeIn(700);
-  $('.star3_2').fadeOut(700, function(){
-    star3_1();
-  });
-}
-function star4_1(){
-  $('.star4_1').css('z-index','2');
-  $('.star4_2').css('z-index','1').fadeIn(500);
-  $('.star4_1').fadeOut(500, function(){
-    star4_2();
-  });
-}
-function star4_2(){
-  $('.star4_2').css('z-index','2');
-  $('.star4_1').css('z-index','1').fadeIn(500);
-  $('.star4_2').fadeOut(500, function(){
-    star4_1();
-  });
-}
-function star5_1(){
-  $('.star5_1').css('z-index','2');
-  $('.star5_2').css('z-index','1').fadeIn(1500);
-  $('.star5_1').fadeOut(1500, function(){
-    star5_2();
-  });
-}
-function star5_2(){
-  $('.star5_2').css('z-index','2');
-  $('.star5_1').css('z-index','1').fadeIn(1500);
-  $('.star5_2').fadeOut(1500, function(){
-    star5_1();
-  });
-}
-function star6_1(){
-  $('.star6_1').css('z-index','2');
-  $('.star6_2').css('z-index','1').fadeIn(2000);
-  $('.star6_1').fadeOut(2000, function(){
-    star6_2();
-  });
-}
-function star6_2(){
-  $('.star6_2').css('z-index','2');
-  $('.star6_1').css('z-index','1').fadeIn(2000);
-  $('.star6_2').fadeOut(2000, function(){
-    star6_1();
-  });
-}
-function star7_1(){
-  $('.star7_1').css('z-index','2');
-  $('.star7_2').css('z-index','1').fadeIn(1800);
-  $('.star7_1').fadeOut(1800, function(){
-    star7_2();
-  });
-}
-function star7_2(){
-  $('.star7_2').css('z-index','2');
-  $('.star7_1').css('z-index','1').fadeIn(1800);
-  $('.star7_2').fadeOut(1800, function(){
-    star7_1();
-  });
-}
-function star9_1(){
-  $('.star9_1').css('z-index','2');
-  $('.star9_2').css('z-index','1').fadeIn(1400);
-  $('.star9_1').fadeOut(1400, function(){
-    star9_2();
-  });
-}
-function star9_2(){
-  $('.star9_2').css('z-index','2');
-  $('.star9_1').css('z-index','1').fadeIn(1400);
-  $('.star9_2').fadeOut(1400, function(){
-    star9_1();
-  });
-}
+
+// ------------------------------------------
+
 function sunY_high(){
   $('.sunY_low').fadeOut(2000);
   $('.sunY_high').fadeIn(2000, function(){
@@ -127,6 +120,23 @@ function sunY_low(){
     sunY_high();
   });
 }
+
+
+
+// function setCloudAnimation(cloudClass, animateTime){
+//   setInterval(function(){
+//     animateClouds(cloudClass, animateTime);
+//   }, animateTime);
+// }
+
+// function animateClouds(){
+//   var bigCloud = $('.' + cloudClass + '_big');
+//   var mediumCloud = $('.' + cloudClass + '_medium');
+//   var smallCloud = $('.' + cloudClass + '_small');
+//   var microCloud = $('.' + cloudClass + '_micro');
+// }
+
+
 function animatedCloudBig(){
   $(".cloud_big").animate({left: '500px'}, 27000, 'linear', function(){
     $('.cloud_big').css({left:-200});
@@ -153,63 +163,36 @@ function animatedCloudMicro(){
 }
 
  
-setInterval( function() {
-  var seconds = new Date().getSeconds();
-  var sdegree = seconds * 6;
-  var srotate = "rotate(" + sdegree + "deg)";
-  // console.log("srotate :", srotate);
-  $("#sec").css({ "transform": srotate });
-
-}, 1000 );
 // -----------------------------------------------
 
-setInterval( function() {
-  var hours = new Date().getHours();
-  var mins = new Date().getMinutes();
-  var hdegree = hours * 30 + (mins / 2);
-  var hrotate = "rotate(" + hdegree + "deg)";
+var Aquarius = [];
+var Pisces;
+var Aries;
+var Taurus;
+var Gemini;
+var Cancer;
+var Leo;
+var Virgo;
+var Libra;
+var Scorpio;
+var Sagittarius;
+var Capricorn;
 
-  $("#hour").css({ "transform": hrotate});
-    
-}, 1000 );
-// -----------------------------------------------
 
-setInterval( function() {
-  var mins = new Date().getMinutes();
-  var mdegree = mins * 6;
-  var mrotate = "rotate(" + mdegree + "deg)";
-  var number = mrotate.replace(/[^0-9]/g, '');
 
-  $("#min").css({ "transform" : mrotate });
-  $("#gradientDisc").css({ "transform": mrotate });
-  console.log( number);
+var date = new Date().getDate();
+var month = new Date().getMonth() + 1;
+console.log(date);
+console.log(month);
 
-}, 1000 );
-// -----------------------------------------------
 
-setInterval( function() {
-  var mins = new Date().getMinutes();
-  var mdegree = mins * 6;
-  var mrotate = "rotate(" + mdegree + "deg)";
-   // remove string from 'mrotate' and keep integer
-  var number = mrotate.replace(/[^0-9]/g, '');
+// if (true) {
 
-  if (number >= 120 && number <= 270){
-    $(".shootingStar").animate({left: '-100', top: '140'}, 700, 'linear', function(){
-      $('.shootingStar').css({left:467, top:437});
-    });
-  }
-}, 20000 );
+// } else if {
 
-// function shootingStar(){
-//   $('.shootingStar').css({left:467, top:437}).delay(30000, function(){
-//     $(".shootingStar").animate({left: '-100', top: '140'}, 1000, 'linear');
-//     // shootingStar();
-//   });
-// // console.log('shoot!');
-// }
- 
-// -----------------------------------------------
+// };
+
+
 
 
 // window.requestAnimationFrame = window.requestAnimationFrame
@@ -237,17 +220,26 @@ setInterval( function() {
 
 
 
+setStarTwinkleInterval('star1', 1000);
+setStarTwinkleInterval('star2', 1300);
+setStarTwinkleInterval('star3', 700);
+setStarTwinkleInterval('star4', 500);
+setStarTwinkleInterval('star5', 1500);
+setStarTwinkleInterval('star6', 2000);
+setStarTwinkleInterval('star7', 1800);
+// setStarTwinkleInterval('star8', 1400);
+setStarTwinkleInterval('star9', 1400);
 
 
 
-star1_1();
-star2_1();
-star3_1();
-star4_1();
-star5_1();
-star6_1();
-star7_1();
-star9_1();
+// setCloudAnimation('.cloud_big', 27000);
+// setCloudAnimation('.cloud_medium', 35000);
+// setCloudAnimation('.cloud_small', 50000);
+// setCloudAnimation('.cloud_micro', 70000);
+
+
+
+
 sunY_high();
 animatedCloudBig();
 animatedCloudSmall();
